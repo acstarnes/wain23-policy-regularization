c452045d938d7a21cd52db42445020fcc6778c03
sukiboo <dereventsov@gmail.com>
2023-09-05 21:41:42 -0400

    make networks larger

--- agent.py
+++ agent.py
@@ -121,8 +121,10 @@ class Agent:
         # construct the model
         self.model = tf.keras.Sequential(name=self.name)
         self.model.add(tf.keras.layers.Flatten())
-        self.model.add(tf.keras.layers.Dense(32, activation='relu'))
-        self.model.add(tf.keras.layers.Dense(32, activation='relu'))
+        ##self.model.add(tf.keras.layers.Dense(32, activation='relu'))
+        ##self.model.add(tf.keras.layers.Dense(32, activation='relu'))
+        self.model.add(tf.keras.layers.Dense(256, activation='relu'))
+        self.model.add(tf.keras.layers.Dense(256, activation='relu'))
         self.model.add(tf.keras.layers.Dense(self.env.num_classes, activation='linear'))
 
         # compile and evaluate the model
--- run_search.sh
+++ run_search.sh
@@ -8,12 +8,12 @@
 #python3 -m main -c cifar10_search_pg_ent
 #python3 -m main -c cifar10_search_pg_l1
 #python3 -m main -c cifar10_search_pg_l2
-#python3 -m main -c cifar100_search_pg_mmd
-#python3 -m main -c cifar100_search_pg_ent
-#python3 -m main -c cifar100_search_pg_l1
-#python3 -m main -c cifar100_search_pg_l2
 python3 -m main -c spotify_search_pg_mmd
 python3 -m main -c spotify_search_pg_ent
 python3 -m main -c spotify_search_pg_l1
 python3 -m main -c spotify_search_pg_l2
+python3 -m main -c cifar100_search_pg_mmd
+python3 -m main -c cifar100_search_pg_ent
+python3 -m main -c cifar100_search_pg_l1
+python3 -m main -c cifar100_search_pg_l2
 
