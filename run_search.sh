2475689a0ede9137d44c3f23390c86c5de132300
sukiboo <dereventsov@gmail.com>
2023-09-14 10:15:38 -0400

    change parameters

--- run_search.sh
+++ run_search.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-#python3 -m main -c mnist_pg
-#python3 -m main -c cifar10_pg
-#python3 -m main -c spotify_pg
+python3 -m main -c mnist
+python3 -m main -c cifar10
+python3 -m main -c spotify
 
-python3 -m main -c mnist_search_pg_mmd
-python3 -m main -c mnist_search_pg_ent
-python3 -m main -c mnist_search_pg_l1
-python3 -m main -c mnist_search_pg_l2
-python3 -m main -c cifar10_search_pg_mmd
-python3 -m main -c cifar10_search_pg_ent
-python3 -m main -c cifar10_search_pg_l1
-python3 -m main -c cifar10_search_pg_l2
-python3 -m main -c spotify_search_pg_mmd
-python3 -m main -c spotify_search_pg_ent
-python3 -m main -c spotify_search_pg_l1
-python3 -m main -c spotify_search_pg_l2
+#python3 -m main -c mnist_search_mmd
+#python3 -m main -c mnist_search_ent
+#python3 -m main -c mnist_search_l1
+#python3 -m main -c mnist_search_l2
+#python3 -m main -c cifar10_search_mmd
+#python3 -m main -c cifar10_search_ent
+#python3 -m main -c cifar10_search_l1
+#python3 -m main -c cifar10_search_l2
+#python3 -m main -c spotify_search_mmd
+#python3 -m main -c spotify_search_ent
+#python3 -m main -c spotify_search_l1
+#python3 -m main -c spotify_search_l2
 
